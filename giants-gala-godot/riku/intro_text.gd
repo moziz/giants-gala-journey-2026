56f91f7f55eec8832175d867a7a5b8ef60e9837a
Raattis <raattis@gmail.com>
2026-01-31 20:57:48 +0200

    mallit, ja outline tekstiin

--- giants-gala-godot/riku/intro_text.gd
+++ giants-gala-godot/riku/intro_text.gd
@@ -10,14 +10,14 @@ func _ready():
 
 func _process(delta):
 	if Jattilaiset.end_countdown > 5.0:
-		label_settings.font_color.a *= 0.99
-		label_settings.font_color.a += 0.01
+		self_modulate.a *= 0.99
+		self_modulate.a += 0.01
 		text = "Wow! You did this!\n\n\n\n\n\n\n"
 		label_settings.font_size = 48
 	elif Jattilaiset.INTRO:
 		intro(delta)
 	elif Jattilaiset.LOPPU:
-		label_settings.font_color.a *= 0.9
+		self_modulate.a *= 0.9
 	elif Jattilaiset.LOPUN_ALKU:
 		visible = true
 		outro(delta)
@@ -36,9 +36,9 @@ func intro(_delta: float):
 		text = "MASK!"
 		var end := (prog - 1.0) / END_PORTION
 		label_settings.font_size = 8 + 512 * end
-		label_settings.font_color.a = 1 - end
+		self_modulate.a = 1 - end
 
 func outro(delta: float):
 	label_settings.font_size = label_settings.font_size * 0.8 + 32 * 0.2
-	label_settings.font_color.a = label_settings.font_color.a * 0.8 + 1.0 * 0.2
+	self_modulate.a = self_modulate.a * 0.8 + 1.0 * 0.2
 	text = "DONE!"
